5e92e65724ac61196cf984c08c149f4c9a3e2745
jrtitanfall2027 <rjrreginaldojunior2008@hotmail.com>
2025-06-15 22:49:25 -0300

    UpDate02

--- contact/admin.py
+++ contact/admin.py
@@ -2,6 +2,33 @@
 
 # # Register your models here.
 
+
+
+
+
+# from django.contrib import admin
+
+# from contact import models
+
+
+# @admin.register(models.Contact)
+# class ContactAdmin(admin.ModelAdmin):
+#     list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
+#     ordering = '-id',
+#     # list_filter = 'created_date',
+#     search_fields = 'id', 'first_name', 'last_name',
+#     list_per_page = 10
+#     list_max_show_all = 200
+#     list_editable = 'first_name', 'last_name', 'show',
+#     list_display_links = 'id', 'phone',
+
+
+   # @admin.register(models.Category)
+   # class CategoryAdmin(admin.ModelAdmin):
+   #     list_display = 'name',
+   #     ordering = '-id',
+
+
 from django.contrib import admin
 
 from contact import models
@@ -9,17 +36,21 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
+    list_display = 'id', 'first_name', 'last_name', 'phone',
     ordering = '-id',
     # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'first_name', 'last_name', 'show',
+    list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone',
-
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = 'name',
     ordering = '-id',
+    # search_fields = 'name',
+    # list_per_page = 10
+    # list_max_show_all = 200
+    # list_editable = 'name',
+    # list_display_links = 'name',
--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,17 @@ Including another URLconf
 # ]
 
 
+# from django.contrib import admin
+# from django.urls import include, path
+
+# urlpatterns = [
+#     path('', include('contact.urls')),
+#     path('admin/', admin.site.urls),
+# ]
+
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -29,3 +40,6 @@ urlpatterns = [
     path('', include('contact.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
