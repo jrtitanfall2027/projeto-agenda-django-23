1c2ef89d3160a77961770322b0bda0500eaac42e
jrtitanfall2027 <rjrreginaldojunior2008@hotmail.com>
2025-06-16 05:24:51 -0300

    UpDate03

--- contact/models.py
+++ contact/models.py
@@ -31,6 +31,9 @@
 #     def __str__(self) -> str:
 #         return f'{self.first_name} {self.last_name}'
 
+
+
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -43,7 +46,14 @@ from django.utils import timezone
 # owner (foreign key)
 
 
+
+
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -64,6 +74,11 @@ class Contact(models.Model):
         on_delete=models.SET_NULL,
         blank=True, null=True
     )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )    
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
